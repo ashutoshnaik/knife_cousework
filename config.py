3b4041b88f2342813c00f50b452f0020415c2ec2
Ashutosh Naik <ashutosh.naik@gmail.com>
2023-12-01 08:16:04 +0000

    epoch 2
    Signed-off-by: Ashutosh Naik <ashutosh.naik@gmail.com>

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ class DefaultConfigs(object):
     img_weight = 224  ## image width
     img_height = 224  ## image height
     batch_size = 16 ## batch size
-    epochs = 20    ## epochs
+    epochs = 5    ## epochs
     learning_rate=0.00005  ## learning rate
 config = DefaultConfigs()
